7a3e0ec90a87a78dad24e2b72ae060148e046584
Your Name <you@example.com>
2026-03-24 19:07:49 +0530

    initial commit

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,4 +1,29 @@
 package base;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BaseTest {
+
+    public static WebDriver driver;
+
+    public void setup(){
+
+        WebDriverManager.chromedriver().setup();
+
+        driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://www.saucedemo.com");
+
+    }
+
+    public void tearDown(){
+
+        driver.quit();
+
+    }
+
 }
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -4,19 +4,43 @@ import base.BaseTest;
 import pages.LoginPage;
 import org.testng.annotations.Test;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.io.FileHandler;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.io.File;
+import java.time.Duration;
+
 public class LoginTest extends BaseTest {
 
     @Test
-    public void validLogin(){
+    public void validLogin() throws Exception {
 
         setup();
+
+        // 📸 1. Screenshot BEFORE login (login page)
+        File loginSrc = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        File loginDest = new File("screenshots/login.png");
+        FileHandler.copy(loginSrc, loginDest);
 
         LoginPage login = new LoginPage(driver);
 
         login.login("standard_user","secret_sauce");
 
+        // ⏳ Wait for successful login page
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.className("inventory_list")
+        ));
+
+        // 📸 2. Screenshot AFTER login (success page)
+        File successSrc = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        File successDest = new File("screenshots/success.png");
+        FileHandler.copy(successSrc, successDest);
+
         tearDown();
-
     }
-
 }
